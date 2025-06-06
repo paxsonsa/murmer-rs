fb6a39a5b1ecc776e03114d3a321dcac97884bb5
Andrew Paxson <andrew.paxson@icloud.com>
2025-06-06 16:53:46 -0700

    chore: update documentation and remove unneeded files

--- murmer/src/node/actor.rs
+++ murmer/src/node/actor.rs
@@ -1,5 +1,12 @@
-// TODO: Handle Joining
-// TODO: Handling Heartbeats
+// TODO: Handle the actor lifecycle properly, including starting, stopping, and error handling.
+// TODO: Handle Leave
+// TODO: Handle Info
+// TODO: Handle ActorAdd/ActorRemove
+// -- ActorAdd: Create a new remote endpoint for the actor and add it the receptionist
+// -- ActorRemove: Remove the remote endpoint for the actor and remove it from the receptionist
+// TODO: Send ActorAdd/ActorRemove messages to the remote node when actors are added or removed to
+// the receptionist.
+// Do we need to consider having a receptionist public status?
 // Actor <- -> Node <- -> Node <- -> Actor
 use std::{fmt::Debug, time::Duration};
 
@@ -76,16 +83,58 @@ pub enum NodeState {
     },
 }
 
+/// The message payloads used for communication between nodes in the cluster.
+///
+/// This enum defines the different types of messages that can be sent and received
+/// between nodes in the cluster.
+///
+/// In a connection there are is a sender and receiver which we will call the connector and
+/// acceptor. The connector is the node that initiates the connection, while the acceptor is the node that
+/// accepts the connection.
+///
+/// 1) The `Initialize` payload is sent by the connector to initiate the connection.
+/// 2) The `Join` payload is sent by the acceptor to join the cluster.
+/// 3) The `JoinAck` payload is sent by the connector to acknowledge the join request. At this
+///    point the link is considered established and the connector can start sending messages to the
+///    acceptor.
+///
+/// **Heartbeats**
+/// Both the connector and acceptor send heartbeats to each other to ensure that the connection is
+/// still alive. It is important that both nodes send heartbeats to each other, as this allows
+/// the nodes to detect if the other node is still reachable. The heartbeat controls the
+/// reachability status of the node, which is used to determine if the node is healthy enough to
+/// communicate with.
+///
+/// **Leave**
+/// The `Leave` payload can be sent by either the connector or acceptor to indicate that they are
+/// leaving the cluster. This payload is used to gracefully close the connection and inform the
+/// other node that it is no longer part of the cluster.
+///
+/// **Info**
+/// The `Info` payload can be used to send information about the node, such as its capabilities and
+/// certain metadata.
+///
+/// **ActorAdd/ActorRemove**
+/// These payloads are used to add or remove actors from the node. These payloads are used to
+/// exchange the current actor state between the nodes in the cluster and ensure that any
+/// publically accessible actors are available on both nodes. These should be exchanged by both
+/// sides of the connection.
+///
+///
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub(super) enum Payload {
+    /// Represents the initialization payload sent when establishing a connection.
     Initialize,
+    /// Represents a message sent to join the cluster.
     Join,
+    /// Represents an acknowledgment of a join request.
     JoinAck,
-    Leave,
+    /// Represents a heartbeat message to check the health of the connection.
     Heartbeat,
+    Leave,
+    Info,
     ActorAdd,
     ActorRemove,
-    Info,
 }
 
 pub(super) enum ConnectionState {
